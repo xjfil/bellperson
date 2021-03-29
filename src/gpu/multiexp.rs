edf12d25a75770bc369ae55f0e4cb52e57e335c9
lavrock <freewenjie@gmail.com>
2021-03-29 14:28:59 +0800

    update

--- src/gpu/multiexp.rs
+++ src/gpu/multiexp.rs
@@ -321,16 +321,16 @@ where
                                 let mut jack_chunk = kern.n;
                                 let size_result = std::mem::size_of::<<G as CurveAffine>::Projective>();
                                 info!("GABEDEBUG: start size_result:{}, jack_chunk:{},", size_result,jack_chunk);
-                                if size_result > 144 {
-                                    jack_chunk = (jack_chunk as f64 / 15f64).ceil() as usize;
-                                    info!("GABEDEBUG: >144 size_result:{}, jack_chunk:{},", size_result,jack_chunk);
-                                }else if size_result == 144 { //3090
-                                    jack_chunk = (jack_chunk as f64 / 2.4f64).ceil() as usize;
-                                    info!("GABEDEBUG: ==144 size_result:{}, jack_chunk:{},", size_result, jack_chunk);
-                                }else{
-                                    jack_chunk = (jack_chunk as f64 / 1.2f64).ceil() as usize;
-                                    info!("GABEDEBUG: <144 size_result:{}, jack_chunk:{},", size_result,jack_chunk);
-                                }
+//                                if size_result > 144 {
+//                                    jack_chunk = (jack_chunk as f64 / 15f64).ceil() as usize;
+//                                    info!("GABEDEBUG: >144 size_result:{}, jack_chunk:{},", size_result,jack_chunk);
+//                                }else{
+//                                    jack_chunk = (jack_chunk as f64 / 1.2f64).ceil() as usize;
+//                                    info!("GABEDEBUG: <144 size_result:{}, jack_chunk:{},", size_result,jack_chunk);
+//                                }
+                                jack_chunk = 45005397 as usize;
+                                info!("GABEDEBUG: <144 size_result:{}, jack_chunk:{},", size_result,jack_chunk);
+
                                 info!("GABEDEBUG: end size_result:{}, jack_chunk:{},", size_result,jack_chunk);
                                 for (bases, exps) in bases.chunks(jack_chunk).zip(exps.chunks(jack_chunk)) {
                                     let result = kern.multiexp(bases, exps, bases.len())?;
